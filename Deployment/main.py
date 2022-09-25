4934baf9ebfc6b0a06c9c9c72ed207427ebdf09f
dev-tyta <hardeykoryar7@gmail.com>
2022-09-25 22:46:32 +0100

    complete pages with file creation

--- Deployment/main.py
+++ Deployment/main.py
@@ -37,7 +37,7 @@ def login():
 
 @app.route("/signup")
 def signup():
-    return render_template("")
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
