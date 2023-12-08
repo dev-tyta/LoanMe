4cea384a758e30c111a1c465c1b74f4927cb8dc7
Testimony Adekoya <hardeykoryar7@gmail.com>
2023-12-08 02:23:53 +0100

    refactoring flask code

--- main.py
+++ main.py
@@ -1,120 +1,96 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
+from models import db, User, ApplicationData
 import pickle
 import numpy as np
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///loan_me.db"
-db = SQLAlchemy(app)
+db.init_app(app)
 
+# Load Pickle Files
+with open('voting_ensemble.pickle', 'rb') as voting_pickle:
+    voting_clf = pickle.load(voting_pickle)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    user = db.Column(db.String(50))
-    email = db.Column(db.String(150))
-    telephone = db.Column(db.Integer())
-    password = db.Column(db.String(70))
-
-    def __repr__(self):
-        return "Contact" + str(self.id)
-
-
-class ApplicationData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gender = db.Column(db.String(10))
-    married = db.Column(db.String(10))
-    dependent = db.Column(db.String(5))
-    self_employed = db.Column(db.String(20))
-    educated = db.Column(db.String(50))
-    income = db.Column(db.Integer)
-    co_income = db.Column(db.Integer)
-    loan_amount = db.Column(db.Imteger)
-    loan_term = db.Column(db.Interger)
-    prop = db.Column(db.String(20))
-
-    def __repr__(self):
-        return "Data" + str(self.id)
-
-
-voting_pickle = open('voting_ensemble.pickle', 'rb')
-map_pickle = open('output_result.pickle', 'rb')
-
-voting_clf = pickle.load(voting_pickle)
-unique_mapping = pickle.load(map_pickle)
-
-
-@app.route("/application-page", methods=["get", "post"])
+@app.route("/application-page", methods=["GET", "POST"])
 def display():
-    if request.method == "post":
-        gender = request.form["gender"]
-        married = request.form["married"]
-        dependent = request.form["dependent"]
-        self_employed = request.form["self_employed"]
-        educated = request.form['education']
-        income = request.form["income"]
-        co_income = request.form["co-income"]
-        loan_amount = request.form["loan_amount"]
-        loan_term = request.form["loan_term"]
-        prop = request.form["property"]
+    if request.method == "POST":
+        # Extract and validate form data
+        gender = request.form.get("gender")
+        married = request.form.get("married")
+        dependent = request.form.get("dependent")
+        self_employed = request.form.get("self_employed")
+        educated = request.form.get("education")
+        income = float(request.form.get("income", 0))
+        co_income = float(request.form.get("co_income", 0))
+        loan_amount = float(request.form.get("loan_amount", 0))
+        loan_term = int(request.form.get("loan_term", 0))
+        prop = request.form.get("property")
 
-        application = ApplicationData(gender=gender, married=married, dependent=dependent, self_employed=self_employed,
-                                      educated=educated, income=income, co_income=co_income, loan_amount=loan_amount,
+        # Add data validation here (omitted for brevity)
+
+        # Save to database
+        application = ApplicationData(gender=gender, married=married, dependent=dependent,
+                                      self_employed=self_employed, educated=educated,
+                                      income=income, co_income=co_income, loan_amount=loan_amount,
                                       loan_term=loan_term, prop=prop)
         db.session.add(application)
         db.session.commit()
-        redirect(url_for("results"))
+        return redirect(url_for("results"))
     return render_template("page.html")
-
 
 @app.route("/application-results")
 def results():
-    pred_arr = [ApplicationData.gender[-1], ApplicationData.married[-1], ApplicationData.dependent[-1],
-                ApplicationData.self_employed[-1], ApplicationData.educated[-1], ApplicationData.income[-1],
-                ApplicationData.co_income[-1], ApplicationData.loan_amount[-1], ApplicationData.loan_term[-1],
-                ApplicationData.prop[-1]]
-    pred_arg = np.array(pred_arr)
-    preds = pred_arg.reshape(1, -1)
-    prediction = voting_clf.predict(preds)
-    prediction = round(float(prediction), 2)
-    if prediction == 0:
-        return render_template("failed.html")
+    latest_application = ApplicationData.query.order_by(ApplicationData.id.desc()).first()
+    if latest_application:
+        # Prepare the data for prediction
+        pred_arr = [latest_application.gender, latest_application.married, latest_application.dependent,
+                    latest_application.self_employed, latest_application.educated, latest_application.income,
+                    latest_application.co_income, latest_application.loan_amount, latest_application.loan_term,
+                    latest_application.prop]
+        pred_arr = np.array(pred_arr).reshape(1, -1)
+        prediction = voting_clf.predict(pred_arr)
+        prediction = round(float(prediction), 2)
+
+        # Render results
+        if prediction == 0:
+            return render_template("failed.html")
+        else:
+            return render_template("results.html", prediction=prediction)
     else:
-        return render_template("results.html", prediction=prediction)
+        return "No application data found"
 
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/login", methods=["get", "post"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'post':
-        mail = request.form["email"]
-        password = request.form["password"]
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        log_in = User.query.filter_by(mail=mail, password=password).first()
-        if log_in is not None:
-            redirect(url_for("homepage"))
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for("homepage"))  # Define 'homepage' route
+        else:
+            return "Invalid login credentials"
     return render_template("login.html")
 
-
-@app.route("/signup", methods=["get", "post"])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.method == "post":
-        name = request.form['full-name']
-        user = request.form['username']
-        mail = request.form['email']
-        telephone = request.form['telephone']
-        password = request.form['password']
+    if request.method == "POST":
+        name = request.form.get('full-name')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        telephone = request.form.get('telephone')
+        password = request.form.get('password')
 
-        sign_up = User(name=name, user=user, mail=mail, telephone=telephone, password=password)
-        db.session.add(sign_up)
+        hashed_password = generate_password_hash(password)
+
+        user = User(name=name, username=username, email=email, telephone=telephone, password=hashed_password)
+        db.session.add(user)
         db.session.commit()
+        return redirect(url_for("login"))  # Redirect to login after signup
     return render_template("signup.html")
 
-
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
