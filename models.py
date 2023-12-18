8a521bb32e56884b55fdd96c78fd0cfa9a7445a0
dev-tyta <hardeykoryar7@gmail.com>
2023-12-18 18:14:21 +0100

    Making commits for 18th

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -12,6 +13,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
 
 class ApplicationData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
